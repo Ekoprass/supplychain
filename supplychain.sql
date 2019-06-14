7404c40926a5b6b43891a18449ce1e311bea51c8
Eko Prasetyo Kurniawan <ekop22496@gmail.com>
2019-06-14 15:08:04 +0700

    sql

---
+++ supplychain.sql
@@ -0,0 +1,124 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Local MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 100134
+ Source Host           : localhost:3306
+ Source Schema         : supplychain
+
+ Target Server Type    : MySQL
+ Target Server Version : 100134
+ File Encoding         : 65001
+
+ Date: 14/06/2019 15:06:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for arsip
+-- ----------------------------
+DROP TABLE IF EXISTS `arsip`;
+CREATE TABLE `arsip`  (
+  `no_dokumen` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `no_po` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `deskripsi` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `jurubeli` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `proyek` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `vendor` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `rak_ke` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `petugas` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tgl_entry` date NULL DEFAULT NULL,
+  `dokumen` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`no_dokumen`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for jurubeli
+-- ----------------------------
+DROP TABLE IF EXISTS `jurubeli`;
+CREATE TABLE `jurubeli`  (
+  `kd_jurubeli` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama_jurubeli` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`kd_jurubeli`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for menus
+-- ----------------------------
+DROP TABLE IF EXISTS `menus`;
+CREATE TABLE `menus`  (
+  `id` int(11) NOT NULL,
+  `menu` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `levels` int(11) NULL DEFAULT NULL,
+  `parent` int(11) NULL DEFAULT NULL,
+  `users` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `icon` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of menus
+-- ----------------------------
+INSERT INTO `menus` VALUES (1, 'DASHBOARD', 1, 0, '1', 'ti-dashboard', 'home');
+INSERT INTO `menus` VALUES (2, 'MASTER DATA', 1, 0, '2', NULL, NULL);
+INSERT INTO `menus` VALUES (3, 'Juru Beli', 2, 2, '2', 'fa fa-briefcase', 'JuruBeli');
+INSERT INTO `menus` VALUES (4, 'Proyek', 2, 2, '2', 'fa fa-gears', 'Proyek');
+INSERT INTO `menus` VALUES (5, 'Vendor', 2, 2, '2', 'fa fa-building-o', 'Vendor');
+INSERT INTO `menus` VALUES (6, 'Rak', 2, 2, '2', 'ti-harddrives', 'Rak');
+INSERT INTO `menus` VALUES (7, 'User', 2, 2, '2', 'fa fa-group', 'User');
+INSERT INTO `menus` VALUES (8, 'ARSIP DOKUMEN', 1, 0, '1', NULL, NULL);
+INSERT INTO `menus` VALUES (9, 'Input Arsip', 2, 8, '1', 'fa fa-download', 'InputArsip');
+INSERT INTO `menus` VALUES (10, 'Data Asip', 2, 8, '1', 'fa fa-archive', 'DataArsip');
+
+-- ----------------------------
+-- Table structure for petugas
+-- ----------------------------
+DROP TABLE IF EXISTS `petugas`;
+CREATE TABLE `petugas`  (
+  `no_petugas` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama_petugas` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `hak_akses` varchar(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`no_petugas`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of petugas
+-- ----------------------------
+INSERT INTO `petugas` VALUES ('030021', 'Bambang P', 'bambangp', '21232F297A57A5A743894A0E4A801FC3', '2');
+
+-- ----------------------------
+-- Table structure for proyek
+-- ----------------------------
+DROP TABLE IF EXISTS `proyek`;
+CREATE TABLE `proyek`  (
+  `kd_proyek` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama_proyek` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`kd_proyek`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for rak_ke
+-- ----------------------------
+DROP TABLE IF EXISTS `rak_ke`;
+CREATE TABLE `rak_ke`  (
+  `no_rak` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `rak_ke` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`no_rak`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for vendor
+-- ----------------------------
+DROP TABLE IF EXISTS `vendor`;
+CREATE TABLE `vendor`  (
+  `kd_vendor` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama_vendor` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`kd_vendor`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
