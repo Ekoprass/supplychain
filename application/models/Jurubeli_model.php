31a48457e1a35f58381322f9926c210fc7598667
KDraxler <39791072+KDraxler@users.noreply.github.com>
2019-06-14 14:31:38 +0700

    Create Jurubeli_model.php
    iki seng bener (2)

---
+++ application/models/Jurubeli_model.php
@@ -0,0 +1,17 @@
+<?php
+class Jurubeli_model extends CI_Model{
+
+  function get_all_jb() 
+  {
+    $this->datatables->select('kd_jurubeli,nama_jurubeli');
+    $this->datatables->from('jurubeli');
+    $this->datatables->add_column('view',
+    '<a href="javascript:void(0);" class="edit_record btn btn-info btn-s" data-kd_jurubeli="$1" data-nama_jurubeli="$2">Edit <i class="fa fa-edit"></a>',
+    'kd_jurubeli,nama_jurubeli');
+        
+    return $this->datatables->generate();
+  }
+
+}
+
+   
