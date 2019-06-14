8aa555149a489ab42630b4e041bd096cf779f463
KDraxler <39791072+KDraxler@users.noreply.github.com>
2019-06-14 11:11:52 +0700

    Create crud_view.php
    Sek beta lur (3)

---
+++ application/views/crud_view.php
@@ -0,0 +1,183 @@
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<title>JURU BELI</title>
+  <link href="<?php echo base_url().'assets/css/bootstrap.css'?>" rel="stylesheet" type="text/css"/>
+	<link href="<?php echo base_url().'assets/css/jquery.datatables.min.css'?>" rel="stylesheet" type="text/css"/>
+  <link href="<?php echo base_url().'assets/css/dataTables.bootstrap.css'?>" rel="stylesheet" type="text/css"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+</head>
+<body>
+  <div class="container">
+    <h2>Juru Beli</h2>
+		<button class="btn btn-success" data-toggle="modal" data-target="#myModalAdd">Add New</button>
+    <table class="table table-striped" id="mytable">
+      <thead>
+        <tr>
+          <th>Kode Juru Beli</th>
+          <th>Juru Beli</th>
+          <!-- <th>Product Code</th>
+          <th>Product Name</th>
+          <th>Price</th>
+          <th>Category</th>-->
+          <th>Action</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+
+	<!-- Modal Add Produk-->
+	  <form id="add-row-form" action="<?php echo base_url().'index.php/crud/simpan'?>" method="post">
+	     <div class="modal fade" id="myModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	        <div class="modal-dialog">
+	           <div class="modal-content">
+	               <div class="modal-header">
+	                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	                   <h4 class="modal-title" id="myModalLabel">Add New</h4>
+	               </div>
+	               <div class="modal-body">
+	                   <div class="form-group">
+	                       <input type="text" name="Kode" class="form-control" placeholder="Kode Juru Beli" required>
+	                   </div>
+										 <div class="form-group">
+	                       <input type="text" name="Nama" class="form-control" placeholder="Nama Juru Beli" required>
+	                   </div>
+	               </div>
+	               <div class="modal-footer">
+	                   	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	                  	<button type="submit" id="add-row" class="btn btn-success">Save</button>
+	               </div>
+	      			</div>
+	        </div>
+	     </div>
+	 </form>
+
+	 <!-- Modal Update Produk-->
+ 	  <form id="add-row-form" action="<?php echo base_url().'index.php/crud/update'?>" method="post">
+ 	     <div class="modal fade" id="ModalUpdate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+ 	        <div class="modal-dialog">
+ 	           <div class="modal-content">
+ 	               <div class="modal-header">
+ 	                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+ 	                   <h4 class="modal-title" id="myModalLabel">Update</h4>
+ 	               </div>
+ 	               <div class="modal-body">
+ 	                   <div class="form-group">
+ 	                       <input type="text" name="Kode" class="form-control" placeholder="Kode Juru Beli" required>
+ 	                   </div>
+ 										 <div class="form-group">
+ 	                       <input type="text" name="Nama" class="form-control" placeholder="Nama Juru Beli" required>
+ 	                   </div>
+ 	               </div>
+ 	               <div class="modal-footer">
+ 	                   	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+ 	                  	<button type="submit" id="add-row" class="btn btn-success">Update</button>
+ 	               </div>
+ 	      			</div>
+ 	        </div>
+ 	     </div>
+ 	 </form>
+
+	 <!-- Modal Hapus Produk-->
+ 	  <form id="add-row-form" action="<?php echo base_url().'index.php/crud/delete'?>" method="post">
+ 	     <div class="modal fade" id="ModalHapus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+ 	        <div class="modal-dialog">
+ 	           <div class="modal-content">
+ 	               <div class="modal-header">
+ 	                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+ 	                   <h4 class="modal-title" id="myModalLabel">Hapus</h4>
+ 	               </div>
+ 	               <div class="modal-body">
+ 	                       <input type="hidden" name="Kode" class="form-control" placeholder="Kode Jurubeli" required>
+												 <strong>Anda yakin mau menghapus record ini?</strong>
+ 	               </div>
+ 	               <div class="modal-footer">
+ 	                   	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+ 	                  	<button type="submit" id="add-row" class="btn btn-success">Hapus</button>
+ 	               </div>
+ 	      			</div>
+ 	        </div>
+ 	     </div>
+ 	 </form>
+
+<script src="<?php echo base_url().'assets/js/jquery-2.1.4.min.js'?>"></script>
+<script src="<?php echo base_url().'assets/js/bootstrap.js'?>"></script>
+<script src="<?php echo base_url().'assets/js/jquery.datatables.min.js'?>"></script>
+<script src="<?php echo base_url().'assets/js/dataTables.bootstrap.js'?>"></script>
+<!-- Specific Page Vendor -->
+<script src="assets/vendor/select2/select2.js"></script>
+<script src="assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
+<script src="assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
+
+<!-- Examples -->
+<script src="assets/javascripts/tables/examples.datatables.editable.js"></script>
+
+<script>
+	$(document).ready(function(){
+		// Setup datatables
+		$.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
+      {
+          return {
+              "iStart": oSettings._iDisplayStart,
+              "iEnd": oSettings.fnDisplayEnd(),
+              "iLength": oSettings._iDisplayLength,
+              "iTotal": oSettings.fnRecordsTotal(),
+              "iFilteredTotal": oSettings.fnRecordsDisplay(),
+              "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+              "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+          };
+      };
+
+      var table = $("#mytable").dataTable({
+          initComplete: function() {
+              var api = this.api();
+              $('#mytable_filter input')
+                  .off('.DT')
+                  .on('input.DT', function() {
+                      api.search(this.value).draw();
+              });
+          },
+              oLanguage: {
+              sProcessing: "loading..."
+          },
+              processing: true,
+              serverSide: true,
+              ajax: {"url": "<?php echo base_url().'index.php/crud/get_guest_json'?>", "type": "POST"},
+                	columns: [
+												{"data": "kd_jurubeli"},
+												{"data": "nama_jurubeli"},
+                        {"data": "view"}
+                  ],
+          		order: [[1, 'asc']],
+          rowCallback: function(row, data, iDisplayIndex) {
+              var info = this.fnPagingInfo();
+              var page = info.iPage;
+              var length = info.iLength;
+              $('td:eq(0)', row).html();
+          }
+
+      });
+			// end setup datatables
+			// get Edit Records
+			$('#mytable').on('click','.edit_record',function(){
+            var Kode=$(this).data('kd_jurubeli');
+						var Nama=$(this).data('nama_jurubeli');
+            $('#ModalUpdate').modal('show');
+            $('[name="Kode"]').val(Kode);
+						$('[name="Nama"]').val(Nama);
+						
+      });
+			// End Edit Records
+			// get Hapus Records
+			$('#mytable').on('click','.hapus_record',function(){
+            var kode=$(this).data('kd_jurubeli');
+            $('#ModalHapus').modal('show');
+            $('[name="Kode"]').val(kode);
+      });
+			// End Hapus Records
+
+	});
+</script>
+</body>
+</html>
