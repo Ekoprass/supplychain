1e8117f6dbc9c441d4677571c696df9fd7fc3780
KDraxler <39791072+KDraxler@users.noreply.github.com>
2019-06-14 14:54:58 +0700

    Create Rak_model.php
    yeah(2)

---
+++ application/models/Rak_model.php
@@ -0,0 +1,17 @@
+<?php
+class Rak_model extends CI_Model{
+
+  function get_all_rak() 
+  {
+    $this->datatables->select('no_rak,rak_ke');
+    $this->datatables->from('rak_ke');
+    $this->datatables->add_column('view',
+    '<a href="javascript:void(0);" class="edit_record btn btn-info btn-s" data-no_rak="$1" data-rak_ke="$2">Edit <i class="fa fa-edit"></a>',
+    'no_rak,rak_ke');
+        
+    return $this->datatables->generate();
+  }
+
+}
+
+   
