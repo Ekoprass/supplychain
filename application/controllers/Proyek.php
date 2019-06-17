dfa20ad55177d4301b117cf290bf3b5a33d15202
Hilda Lauda Satria <satriahilda3@gmail.com>
2019-06-17 09:53:58 +0700

    controller

---
+++ application/controllers/Proyek.php
@@ -0,0 +1,45 @@
+<?php
+class Proyek extends CI_Controller{
+  function __construct(){
+    parent::__construct();
+    $this->load->library('datatables');
+    $this->load->model('Proyek_model');
+  }
+  function index(){
+    $this->load->view('Proyek_view');
+  }
+
+  function get_guest_json() { //data data produk by JSON object
+    header('Content-Type: application/json');
+    echo $this->Proyek_model->get_all_md();
+  }
+
+
+  function simpan(){ //function simpan data
+    $data=array(
+      'kd_proyek'     => $this->input->post('Kode'),
+      'nama_proyek'     => $this->input->post('Nama'),
+    );
+    $this->db->insert('proyek', $data);
+    redirect('proyek');
+  }
+
+  function update(){ //function update data
+    $kode=$this->input->post('Kode');
+    $data=array(
+      // 'kd_proyek'     => $this->input->post('Kode')
+      'nama_proyek'     => $this->input->post('Nama')
+    );
+    $this->db->where('kd_proyek',$kode);
+    $this->db->update('proyek', $data);
+    redirect('proyek');
+  }
+
+  function delete(){ //function hapus data
+    $kode=$this->input->post('Kode');
+    $this->db->where('kd_proyek',$kode);
+    $this->db->delete('proyek');
+    redirect('proyek');
+  }
+
+}
