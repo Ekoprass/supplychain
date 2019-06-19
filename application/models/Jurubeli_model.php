b8f5399f8a8b8be17d6b682104eeef93014e7118
KDraxler <39791072+KDraxler@users.noreply.github.com>
2019-06-19 08:35:44 +0700

    up

--- application/models/Jurubeli_model.php
+++ application/models/Jurubeli_model.php
@@ -6,7 +6,11 @@ class Jurubeli_model extends CI_Model{
     $this->datatables->select('kd_jurubeli,nama_jurubeli');
     $this->datatables->from('jurubeli');
     $this->datatables->add_column('view',
+<<<<<<< HEAD
     '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-kd_jurubeli="$1" data-nama_jurubeli="$2">Edit &nbsp <i class="fa fa-edit"></a>',
+=======
+    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-kd_jurubeli="$1" data-nama_jurubeli="$2">EDIT &nbsp <i class="fa fa-edit"></a>',
+>>>>>>> 75545f97fa86d1013c34a986b409c027f64f1327
     'kd_jurubeli,nama_jurubeli');
         
     return $this->datatables->generate();
