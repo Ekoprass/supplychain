4d70f071f90410e4555c0f773e15e5e734926576
abizhafar <abizhafar@gmail.com>
2019-06-20 11:02:57 +0700

    new icon position edit 2.0

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -20,7 +20,7 @@ class User_model extends CI_Model{
     $this->datatables->select('no_petugas,nama_petugas,username,password,hak_akses');
     $this->datatables->from('petugas');
     $this->datatables->add_column('view',
-    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-no_petugas="$1" data-nama_petugas="$2" data-username="$3" data-password="$4" data-hak_akses="$5">EDIT &nbsp <i class="fa fa-edit"></a>',
+    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-no_petugas="$1" data-nama_petugas="$2" data-username="$3" data-password="$4" data-hak_akses="$5"><i class="fa fa-edit"> &nbsp EDIT </a>',
     'no_petugas,nama_petugas,username,password,hak_akses');
         
     return $this->datatables->generate();
