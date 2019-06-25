0d7bbcf1ea56512ca5ea42df536944a87a886036
abizhafar <abizhafar@gmail.com>
2019-06-25 13:08:36 +0700

    update user 9.0

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,7 +3,7 @@ class User_model extends CI_Model{
 	
 		public function Login($username,$password)
 		{
-			$this->db->select('no_petugas,username,password,hak_akses,nama_petugas');
+			$this->db->select('no_petugas,nama_petugas,username,password,hak_akses');
 			$this->db->from('petugas');
 			$this->db->where('username', $username);
 			$this->db->where('password', md5($password));
@@ -20,8 +20,8 @@ class User_model extends CI_Model{
     $this->datatables->select('no_petugas,nama_petugas,username,password,hak_akses');
     $this->datatables->from('petugas');
     $this->datatables->add_column('view',
-    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-no_petugas="$1" data-nama_petugas="$2" data-username="$3" data-password="$4" data-hak_akses="$5"><i class="fa fa-edit"> &nbsp EDIT </a>',
-    'no_petugas,nama_petugas,username,password,hak_akses');
+    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-no_petugas="$1" data-nama_petugas="$2" data-username="$3" data-password="$4" data-hak_akses="$5"><i class="fa fa-edit"></i> &nbsp EDIT </a>
+     <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-s" data-kd_jurubeli="$1"><i class="fa fa-trash-o"></i> &nbsp HAPUS</a>', 'no_petugas,nama_petugas,username,password,hak_akses');
         
     return $this->datatables->generate();
   }
