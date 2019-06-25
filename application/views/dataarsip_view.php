de3e3b99edd9bc07f7bd0f86fc6def6b66500d6d
abizhafar <abizhafar@gmail.com>
2019-06-25 14:27:14 +0700

    Update dataarsip_view.php

--- application/views/dataarsip_view.php
+++ application/views/dataarsip_view.php
@@ -43,7 +43,7 @@
      <div class="modal-content">
        <div class="modal-header">
          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-         <h4 class="modal-title" id="myModalLabel">UPDATE DATA USER</h4>
+         <h4 class="modal-title" id="myModalLabel">UPDATE DATA ARSIP</h4>
        </div>
        <div class="modal-body">
          <div class="form-group">
